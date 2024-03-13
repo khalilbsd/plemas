7648cd848ead0012b806497a40e19301d7632c30
khalil ben said <khalilbensaid98@gmail.com>
2024-03-13 09:25:15 +0000

    removing stacks from  response

--- server/Utils/errorHandler.js
+++ server/Utils/errorHandler.js
@@ -1,3 +1,4 @@
+import { config } from "../environment.config.js";
 import logger from "../log/config.js";
 
 const sendErrorDev = (err, res) => {
@@ -8,11 +9,23 @@ const sendErrorDev = (err, res) => {
     stack: err.stack
   });
 };
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    // stack: err.stack
+  });
+};
 
 export const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 //   logger.error(err);
-console.log(err);
-  sendErrorDev(err, res);
+
+  if (!config.env_dev){
+    sendErrorDev(err, res);
+  }else{
+    sendErrorProd(err,res)
+  }
 };
